0ec8e385865ac7ec54bb22a0c86588258a777d27
Son Beomsoo <k1a11220@github.com>
2019-08-05 16:21:29 +0900

    push master

--- todo_App/App.js
+++ todo_App/App.js
@@ -1,48 +1,40 @@
 import React from 'react';
-import { StyleSheet, Text, View, StatusBar, Dimensions } from 'react-native';
-import { TextInput } from 'react-native-gesture-handler';
-import { Platform } from '@unimodules/core';
-import ToDo from "./ToDo.js";
+import { StyleSheet, Text, View, StatusBar, TextInput, Dimensions, Platform } from 'react-native';
 
 const { height, width } = Dimensions.get("window");
 
 export default function App() {
   state = {
     newToDo: ""
-  };
-  
-  const { newToDo } = this.state;
+  }
   return (
     <View style={styles.container}>
-      <StatusBar barStyle="light-content"/>
-      <Text style={styles.title}>To Do!</Text>
+      <StatusBar barStyle="light-content" />
+      <Text style={styles.title}>Awesome To Do</Text>
       <View style={styles.card}>
-        <TextInput style={styles.input} placeholder={"할 일"} value={newToDo} onChangeText={this._controlNewToDo} placeholderTextColor={"#999"} returnKeyType={"입력하기"}/>
+        <TextInput style={styles.input} placeholder={"New To Do"} value={newToDo} onChangeText={this._controlNewToDo} />
       </View>
-      <ScrollView>
-        <ToDo />
-      </ScrollView>
     </View>
   );
-  _controlNewToDo = text => {
-    this.setState({
-      newToDo: text
-    });
-  };
+}
+_controlNewToDo = text => {
+  this.setState({
+    newToDo: text
+  });
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#f23657',
+    backgroundColor: '#F23657',
     alignItems: 'center',
   },
   title: {
     color: "white",
     fontSize: 30,
-    marginTop: 70,
-    fontWeight: "100",
-    marginBottom: 50
+    marginTop: 50,
+    fontWeight: "200",
+    marginBottom: 30
   },
   card: {
     backgroundColor: "white",
@@ -50,21 +42,20 @@ const styles = StyleSheet.create({
     width: width - 25,
     borderTopLeftRadius: 10,
     borderTopRightRadius: 10,
-    // ...Platform.select({
-    //   ios: {
-    //     shadowColor: 'rgb(50, 50, 50)',
-    //     shadowOpacity: 0.5,
-    //     shadowRadius: 5,
-    //     shadowOffset: {
-    //       height: -1,
-    //       width: 0
-    //     },
-    //   },
-    //   android: {
-    //     elevation: 3
-    //   }
-    // })
-    elevation: 3
+    ...Platform.select({
+      ios: {
+        shadowColor: 'rgba(50, 50, 50)',
+        shadowOpacity: 0.5,
+        shadowRadius: 5,
+        shadowOffset: {
+          height: -1,
+          width: 0
+        },
+      },
+      android: {
+        elevation: 3
+      }
+    })
   },
   input: {
     padding: 20,
